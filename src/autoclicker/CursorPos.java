8b2606c41940ba57e9b41cfc8c5dfc49a9990f8d
simonmarty0 <simon.marty0@gmail.com>
2017-12-07 19:33:49 -0500

    Completion of the CursorPos class

--- src/autoclicker/CursorPos.java
+++ src/autoclicker/CursorPos.java
@@ -5,49 +5,52 @@ package autoclicker;
 
 import java.awt.MouseInfo;
 import java.awt.Robot;
+import java.awt.event.InputEvent;
 import java.awt.AWTException;
 
- public class CursorPos {
+public class CursorPos {
 
-    private int width;	// Pixel position on horizontal axis, 0 = Left
-	private int height;	// Pixel position on vertical axis, 0 = Top
-    
-     public void CursorPos()
-     {
-         this.getCursorPos();
-     }
+	private int width; // Pixel position on horizontal axis, 0 = Left
+	private int height; // Pixel position on vertical axis, 0 = Top
 
-     public void updateCursorPos() {
-         try {
+	public CursorPos() {
+		this.updateCursorPos();
+	}
+
+	public void updateCursorPos() {
+		try {
 			width = (int) MouseInfo.getPointerInfo().getLocation().getX();
 			height = (int) MouseInfo.getPointerInfo().getLocation().getY();
-			} catch (Exception e) {
-				System.out.println("Getting mouse pointeer position failed");
-				e.printStackTrace();
-			}
-     }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-     public int getCursorX() {
-         this.updateCursorPos();
-         return width;
-     }
+	public int getCursorX() {
+		return width;
+	}
 
-     public int getCursorY() {
-         this.updateCursorPos();
-         return height;
-     }
-     public void moveCursor(int w, int h)
-     {
-         			Robot robot = null;
-					try {
-						robot = new Robot();
-					} catch (AWTException ex) {
-						ex.printStackTrace();
-					}
-					System.out.println("The action key was pressed");
-					robot.mouseMove(w, h);
-					robot.mousePress(InputEvent.BUTTON1_MASK);
-				    robot.mouseRelease(InputEvent.BUTTON1_MASK);
-					System.out.println("Mouse cursor moved to :" + w + " " + h);
-     }
- }
+	public int getCursorY() {
+		return height;
+	}
+
+	public void moveCursor(int w, int h) {
+		try {
+			Robot robot = new Robot();
+			robot.mouseMove(w, h);
+		} catch (AWTException ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+	public void click() {
+		try {
+			Robot robot = new Robot();
+			robot.mousePress(InputEvent.BUTTON1_MASK);
+			robot.mouseRelease(InputEvent.BUTTON1_MASK);
+		} catch (AWTException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
--- src/autoclicker/Frame.java
+++ src/autoclicker/Frame.java
@@ -8,28 +8,27 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.Font;
-import java.awt.MouseInfo;
-import java.awt.Robot;
-import java.awt.AWTException;
 import java.awt.Color;
 import java.awt.event.ActionListener;
-import java.awt.event.InputEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class Frame1 {
+public class Frame {
 
 	private JFrame frmAutoclicker;
 	public final int RECORD = 75;	// key : k
 	public final int ACTION = 73;	// key : i
+	public CursorPos cursor = new CursorPos();
+	int height = 0;
+	int width = 0;
 
 	public static void main(String[] args) {	//instantiates the JFrame through its constructor
 
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Frame1 window = new Frame1();
+					Frame window = new Frame();
 					window.frmAutoclicker.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -38,7 +37,7 @@ public class Frame1 {
 		});
 	}
 
-	public Frame1() {	//constructor of the frame
+	public Frame() {	//constructor of the frame
 		initialize();
 	}
 
@@ -71,9 +70,11 @@ public class Frame1 {
 			public void keyPressed(KeyEvent arg0) {
 				int code = arg0.getKeyCode(); // get the key code of the key currently pressed
 				if (code == RECORD) {
+					cursor.updateCursorPos();
 				}
 				if (code == ACTION) {	//second keycode, launches the macro
-
+					cursor.moveCursor(cursor.getCursorX(), cursor.getCursorY());
+					cursor.click();
 				}
 
 			}
